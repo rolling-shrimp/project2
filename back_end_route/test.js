b864f03f885beeb793c35c25590369cf359a9fb2
Scott Liu <wbcwbc1616@gamil.com>
2024-02-12 12:53:09 +0800

    add build

---
+++ back_end_route/test.js
@@ -0,0 +1,33 @@
+// class thecssStatic {
+//   constructor(
+//     name,
+//     translateX,
+//     translateY,
+//     thecss,
+//     imageHeight,
+//     imageWidth,
+//     thesrc
+//   ) {
+//     this.name = name;
+//     this.translateX = translateX;
+//     this.translateY = translateY;
+//     this.thecss = thecss;
+//     this.imageHeight = imageHeight;
+//     this.imageWidth = imageWidth;
+//     this.thesrc = thesrc;
+//   }
+// }
+// let arr = [];
+// let lefTop = new thecssStatic(
+//   "leftTop",
+//   [0, 10],
+//   [0, 5],
+//   { padding: 0 },
+//   500,
+//   500,
+//   "/image/kobe.img"
+// );
+// arr.push(lefTop);
+// console.log(arr);
+
+// document.getElementById("btn").onclick=useClosure();
