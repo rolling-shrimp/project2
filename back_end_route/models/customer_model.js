9f9f56687fc089040e026fe5081dee779375c2b5
Scott Liu <wbcwbc1616@gamil.com>
2024-02-29 11:25:24 +0800

    change the mongoose customer model State validate condition

--- back_end_route/models/customer_model.js
+++ back_end_route/models/customer_model.js
@@ -4,7 +4,7 @@ const customerSchema = new Schema({
   CustID: { type: String, minlength: 7, maxlength: 10, required: true },
   Name: { type: String, maxlength: 20, minlength: 2, required: true },
   Country: { type: String, maxlength: 20, minlength: 2, required: true },
-  State: { type: String, maxlength: 20, minlength: 2 },
+  State: { type: String, maxlength: 20, minlength: 0 },
   Zip: { type: Number, max: 999 },
   City: { type: String, maxlength: 20, required: true, minlength: 2 },
   Address: { type: String, maxlength: 100, minlength: 5, required: true },
